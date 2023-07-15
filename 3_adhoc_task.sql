cd85f647e1e19d20c9df62a19865326292733e79
ashwini2306 <136921131+ashwini2306@users.noreply.github.com>
2023-07-15 20:43:18 +0530

    Update 3_adhoc_task.sql

--- 3_adhoc_task.sql
+++ 3_adhoc_task.sql
@@ -2,16 +2,20 @@
 
 ----------1. Get the order id alone from the order_Id column-------------
  select substring(order_id,4,14)  as order_id from sales_purchase_data_updated
+  
  ----------2. order_month, should have month of the order date and year_month should have year of the month-----
  select 
  order_date,
  month (order_date)as order_month,
  year (order_date)as order_year
  from sales_purchase_data_updated  ;
+
  -------------3. Customer id--------
  select substring(customer_id,4,8)  as customer_id from sales_purchase_data_updated
+  
  ----------4. Concatenate Region,Country, city, state as location_info for each records---------
  select concat(region,'_',country,'_',city,'_',state) as location_info from sales_purchase_data_updated 
+  
  ------
  use internship;
  show tables;
@@ -41,6 +45,7 @@ select substring(product_id,5,15) as product_id from sales_order_info
 
 --------5 b‎) per_quantity_price should be  output of Sales / Quantity of each customer records‎-------
 ‎select * from sales_order_info
+ 
 --------5 c)sales_type should be 3 categories ‎---------
 select 
 product_id,
@@ -89,7 +94,7 @@ select count(*) order_id from sales_purchase_data_updated
 select count(*) product_names from sales_order_info
 
 ----------8. Count of distinct Segments in sales_purchase_data_updated table-------------
-select count(*) segments from sales_purchase_data_updated
+select count(distinct segment) from sales_purchase_data_updated
 
 --------9.Recent order date in  sales_purchase_data_updated table-----
 select max(order_date) from  sales_purchase_data_updated 
